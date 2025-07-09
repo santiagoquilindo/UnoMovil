0ae81f8fafe1b83c7345c7758b12232ec2995fd7
santiago quilindo <santiagoquilindo32@gmail.com>
2025-07-09 20:40:40 +0000

    no necesito login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,4 @@
-import { Rocket, User } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Button } from './ui/button';
+import { Rocket } from 'lucide-react';
 
 export function Header() {
   return (
@@ -18,27 +8,6 @@ export function Header() {
           <Rocket className="h-8 w-8" />
           <h1 className="text-xl font-bold tracking-tight">Popayán Go</h1>
         </div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-              <Avatar className="h-10 w-10">
-                <AvatarImage src="https://placehold.co/40x40.png" alt="@user" />
-                <AvatarFallback>
-                  <User />
-                </AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-56">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>History</DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Log out</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
     </header>
   );
