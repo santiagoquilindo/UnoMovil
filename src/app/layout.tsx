b7d1030bdc22fab4dffc29e64f4bfe24dad5d8ed
santiago quilindo <santiagoquilindo32@gmail.com>
2025-07-09 21:09:23 +0000

    este es el logo podrias acomodarlo porque se ve raro donde lo ubicaste c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter&family=Kalam:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased h-full bg-background">
         {children}
